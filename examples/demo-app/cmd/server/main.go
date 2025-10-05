ca4463cef7777266a08606196de4815ed2ffbbb0
marcoschwartz <marcolivier.schwartz@gmail.com>
2025-10-05 10:43:53 +0200

    fixes

---
+++ examples/demo-app/cmd/server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"demo-app/templates"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	// Middleware
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Static files
+	fs := http.FileServer(http.Dir("static"))
+	r.Handle("/static/*", http.StripPrefix("/static/", fs))
+
+	// Routes
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		templates.Home().Render(r.Context(), w)
+	})
+
+	r.Get("/inputs", func(w http.ResponseWriter, r *http.Request) {
+		templates.InputsPage().Render(r.Context(), w)
+	})
+
+	r.Get("/feedback", func(w http.ResponseWriter, r *http.Request) {
+		templates.FeedbackPage().Render(r.Context(), w)
+	})
+
+	r.Get("/navigation", func(w http.ResponseWriter, r *http.Request) {
+		templates.NavigationPage().Render(r.Context(), w)
+	})
+
+	r.Get("/layout", func(w http.ResponseWriter, r *http.Request) {
+		templates.LayoutComponentsPage().Render(r.Context(), w)
+	})
+
+	port := ":3058"
+	log.Printf("Starting server on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, r))
+}
